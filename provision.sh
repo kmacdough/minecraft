6c0dbcabace4a40f3953fd9379ae473eae4026b8
Kevin McDonough <kmacdough@gmail.com>
2021-12-10 15:09:46 -0800

    Simplify & use generic server name

--- provision.sh
+++ provision.sh
@@ -5,14 +5,20 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 
-mc_home=$(su - minecraft -c pwd)
-instance_folder="$mc_home/instances/$1"
-paper_url="https://papermc.io/api/v2/projects/paper/versions/1.18/builds/66/downloads/paper-1.18-66.jar"
+server_url="https://papermc.io/api/v2/projects/paper/versions/1.18/builds/66/downloads/paper-1.18-66.jar"
 
-su - minecraft -c "mkdir -p $instance_folder"
-su - minecraft -c "curl $paper_url > $instance_folder/paper.jar"
-su - minecraft -c "echo eula=true > $instance_folder/eula.txt"
+# Setup server directory
+instance_folder="$(su - minecraft -c pwd)/instances/$1"
+if [ -d "$instance_folder" ]; then
+    echo "Instance already exists. Check status with:"
+    echo "  systemctl status minecraft@$1"
+else
+    mkdir -p "$instance_folder"
+    curl $server_url > "$instance_folder/server.jar"
+    echo eula=true > "$instance_folder/eula.txt"
+    chown -R minecraft:minecraft "$instance_folder"
 
-# Start and enable (start after boot) the server.
-systemctl start "minecraft@$1"
-systemctl enable "minecraft@$1"
+    # Start and enable (start after boot) the server.
+    systemctl start "minecraft@$1"
+    systemctl enable "minecraft@$1"
+fi
